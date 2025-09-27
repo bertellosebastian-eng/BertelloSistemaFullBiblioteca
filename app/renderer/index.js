bfc61c1e0cb679456ca7435b663ef3af8a6bbb5a
bertellosebastian-eng <bertellosebastian@gmail.com>
2025-09-27 15:47:57 +0000

    Preparar empaquetado profesional para Windows con Electron Builder

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -1,5 +1,15 @@
 // Punto de entrada de la interfaz
 window.onload = function() {
 	// Redirige al menú principal al iniciar
-	window.location.href = 'menu.html';
+	const reg = localStorage.getItem('registro');
+	if (!reg) {
+		window.location.href = 'registro.html';
+	} else {
+		const datos = JSON.parse(reg);
+		if (datos.acepto) {
+			window.location.href = 'menu.html';
+		} else {
+			window.location.href = 'registro.html';
+		}
+	}
 };
